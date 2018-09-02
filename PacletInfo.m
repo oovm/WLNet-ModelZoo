6d3a2acc054bbce4ed9ddbae0208b5d53ccde4c1
GalAster <galaster@foxmail.com>
2018-09-02 14:30:32 +0800

    架构::First Blood!

---
+++ PacletInfo.m
@@ -0,0 +1,17 @@
+Paclet[
+  Name -> "GluonCV",
+  Version -> "0.0.1",
+  Extensions -> {
+
+    	{
+
+     		"Kernel",
+
+     		"Root" -> ".",
+
+     		"Context" -> {"GluonCV`"}
+
+     	}
+
+    }
+ ]
