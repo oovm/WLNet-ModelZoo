2bafd4ce4b9b5aca01ab5e451dcaa7889bb4f1dc
GalAster <galaster@foxmail.com>
2018-10-18 16:16:38 +0800

    模型::ResNeXt on CIFAR10 & ImageNet[NotAvailable]

---
+++ Models/ResNeXt on CIFAR10/export.py
@@ -0,0 +1,11 @@
+import gluoncv.model_zoo as gz
+from gluoncv.utils import export_block
+
+def zoo_import(name, head=''):
+	"""Download from Gluoncv Zoo"""
+	net = gz.get_model(name, pretrained=True)
+	export_block(head + name, net, preprocess=True)
+
+# can not download
+# zoo_import('cifar_resnext29_32x4d')
+zoo_import('cifar_resnext29_16x64d')
