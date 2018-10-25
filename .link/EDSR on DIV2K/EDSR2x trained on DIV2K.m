72a5a5db0ed06d3a112123929323a0d8893a4923
GalAster <galaster@foxmail.com>
2018-10-25 13:17:06 +0800

    模型::Enhanced Deep Residual Networks(EDSR) trained on DIV2K

---
+++ .link/EDSR on DIV2K/EDSR2x trained on DIV2K.m
@@ -0,0 +1,112 @@
+(* ::Package:: *)
+
+SetDirectory@NotebookDirectory[];
+Needs["MXNetLink`"]
+Needs["NeuralNetworks`"]
+DateString[]
+
+
+(* ::Subitem:: *)
+(*Thu 25 Oct 2018 13:11:36*)
+
+
+(* ::Subchapter:: *)
+(*Import Weights*)
+
+
+params = NDArrayImport["EDSR_x2-0000.params"];
+
+
+(* ::Subchapter:: *)
+(*Encoder & Decoder*)
+
+
+encoder = NetEncoder[{"Image", {640, 360}}]
+decoder = NetDecoder["Image"]
+
+"Use this encoder if not use shift convolution";
+NetEncoder[{
+	"Image", {640, 360},
+	"MeanImage" -> {0.4488, 0.4371, 0.4040},
+	"VarianceImage" -> 1 / 255^2
+}];
+
+
+(* ::Subchapter::Closed:: *)
+(*Pre-defined Structure*)
+
+
+getBlock[i_] := NetGraph[{
+	ConvolutionLayer[
+		"Weights" -> params["arg:body." <> ToString[i] <> ".body.0.weight"],
+		"Biases" -> params["arg:body." <> ToString[i] <> ".body.0.bias"],
+		"PaddingSize" -> 1, "Stride" -> 1
+	],
+	ElementwiseLayer["ReLU"],
+	ConvolutionLayer[
+		"Weights" -> params["arg:body." <> ToString[i] <> ".body.2.weight"],
+		"Biases" -> params["arg:body." <> ToString[i] <> ".body.2.bias"],
+		"PaddingSize" -> 1, "Stride" -> 1
+	],
+	ThreadingLayer[#1 + 0.1#2&]
+}, {
+	NetPort["Input"] -> 1 -> 2 -> 3,
+	{NetPort["Input"], 3} -> 4
+}];
+$body = NetGraph[{
+	NetChain@Array[getBlock, 31],
+	ConvolutionLayer[
+		"Weights" -> params["arg:body.32.weight"],
+		"Biases" -> params["arg:body.32.bias"],
+		"PaddingSize" -> 1, "Stride" -> 1
+	],
+	ThreadingLayer[Plus]
+}, {
+	NetPort["Input"] -> 1 -> 2,
+	{NetPort["Input"], 2} -> 3
+}];
+
+
+(* ::Subchapter:: *)
+(*Main*)
+
+
+mainNet = NetChain[{
+	ConvolutionLayer[
+		"Weights" -> params["arg:sub_mean.weight"],
+		"Biases" -> params["arg:sub_mean.bias"],
+		"PaddingSize" -> 0, "Stride" -> 1
+	],
+	ConvolutionLayer[
+		"Weights" -> params["arg:head.0.weight"],
+		"Biases" -> params["arg:head.0.bias"],
+		"PaddingSize" -> 1, "Stride" -> 1
+	],
+	$body,
+	ConvolutionLayer[
+		"Weights" -> params["arg:tail.0.0.weight"],
+		"Biases" -> params["arg:tail.0.0.bias"],
+		"PaddingSize" -> 1, "Stride" -> 1
+	],
+	PixelShuffleLayer[2],
+	ConvolutionLayer[
+		"Weights" -> params["arg:tail.1.weight"],
+		"Biases" -> params["arg:tail.1.bias"],
+		"PaddingSize" -> 1, "Stride" -> 1
+	],
+	ConvolutionLayer[
+		"Weights" -> params["arg:add_mean.weight"],
+		"Biases" -> params["arg:add_mean.bias"],
+		"PaddingSize" -> 0, "Stride" -> 1
+	]
+},
+	"Input" -> encoder,
+	"Output" -> decoder
+]
+
+
+(* ::Subchapter:: *)
+(*Export Model*)
+
+
+Export["EDSR2x trained on DIV2K.WXF", mainNet]
--- .link/Xception on ImageNet/exporter.py
+++ .link/Xception on ImageNet/exporter.py
@@ -1,3 +1,12 @@
+from os.path import abspath
+from sys import path
+
+path.append(abspath('../.tools/'))
+import torch
+from pth_exporter import pth2wxf
 from pretrainedmodels import xception
 
-xception(num_classes=1000, pretrained='imagenet')
+# manually fix this first
+net = xception(num_classes=1000, pretrained=False)
+torch.save(net, 'imagenet_xception.pth')
+
