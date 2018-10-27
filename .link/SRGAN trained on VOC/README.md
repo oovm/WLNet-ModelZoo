7f6bfaa7d9ff560bcedb41d3deb1ec703a3a10d0
GalAster <galaster@foxmail.com>
2018-10-27 18:21:23 +0800

    模型::Photo-Realistic Single Image Super-Resolution

---
+++ .link/SRGAN trained on VOC/README.md
@@ -0,0 +1,20 @@
+### Remark
+
+TODO: use single prelu
+
+### Source
+
+https://github.com/WolframRhodium/Super-Resolution-Zoo/tree/master/SRGAN/SRGAN%40leftthomas
+
+---
+
+### Reference
+### Reference
+```TeX
+@ImageSuperResolution{isola2017image,
+  title={Image-to-image translation with conditional adversarial networks},
+  author={Isola, Phillip and Zhu, Jun-Yan and Zhou, Tinghui and Efros, Alexei A},
+  journal={arXiv preprint},
+  year={2017}
+}
+```
---
+++ .link/SRGAN trained on VOC/SRGAN4x trained on VOC.m
@@ -0,0 +1,101 @@
+(* ::Package:: *)
+
+SetDirectory@NotebookDirectory[];
+Needs["MXNetLink`"]
+Needs["NeuralNetworks`"]
+DateString[]
+
+
+(* ::Subitem:: *)
+(*Sat 27 Oct 2018 18:17:37*)
+
+
+(* ::Subchapter:: *)
+(*Import Weights*)
+
+
+params = NDArrayImport["SRGAN_4x-0000.params"];
+
+
+(* ::Subchapter:: *)
+(*Encoder & Decoder*)
+
+
+encoder = NetEncoder[{"Image", {640, 360}}]
+decoder = NetDecoder["Image"]
+
+
+(* ::Subchapter:: *)
+(*Pre-defined Structure*)
+
+
+getCN[i_, s_, p_] := ConvolutionLayer[
+	"Weights" -> params["arg:learned_" <> ToString[i]],
+	"Biases" -> params["arg:learned_" <> ToString[i + 1]],
+	"PaddingSize" -> p, "Stride" -> s
+]
+getBN[i_] := BatchNormalizationLayer[
+	"Epsilon" -> 1*^-5,
+	"Gamma" -> params["arg:learned_" <> ToString[i]],
+	"Beta" -> params["arg:learned_" <> ToString[i + 1]],
+	"MovingMean" -> params["aux:learned_" <> ToString[i + 2]],
+	"MovingVariance" -> params["aux:learned_" <> ToString[i + 3]]
+]
+
+(*Notice that there is no single prelu in WLNet*)
+getPrelu[i_] := ParametricRampLayer[
+	"Slope" -> Flatten@ConstantArray[Normal@ params["arg:learned_" <> ToString[9]], 64]
+]
+
+getBlock[i_] := NetGraph[{
+	getCN[i, 1, 1],
+	getBN[i + 2],
+	getPrelu[i + 6],
+	getCN[i + 7, 1, 1],
+	getBN[i + 9],
+	ThreadingLayer[Plus]
+}, {
+	NetPort["Input"] -> 1 -> 2 -> 3 -> 4 -> 5,
+	{NetPort["Input"], 5} -> 6
+}];
+
+
+$head = NetChain@{
+	getCN[0, 1, 4],
+	getPrelu[2]
+};
+$body = NetGraph[{
+	NetChain@Table[getBlock[i], {i, 3, 55, 13} ],
+	getCN[68, 1, 1],
+	getPrelu[70],
+	ThreadingLayer[Plus]
+}, {
+	NetPort["Input"] -> 1 -> 2 -> 3 ,
+	{NetPort["Input"], 3} -> 4
+}];
+$tail = NetChain@{
+	getCN[71, 1, 1],
+	PixelShuffleLayer[2],
+	getPrelu[73],
+	getCN[74, 1, 1],
+	PixelShuffleLayer[2],
+	getPrelu[76],
+	getCN[77, 1, 4],
+(*LogisticSigmoid[2x]==(Tanh[x]+1)/2*)
+	ElementwiseLayer[(Tanh[#] + 1) / 2&]
+};
+
+
+(* ::Subchapter:: *)
+(*Main*)
+
+
+mainNet = NetChain[{$head, $body, $tail}] // NetFlatten;
+mainNet = NetReplacePart[mainNet, {"Input" -> encoder, "Output" -> decoder}]
+
+
+(* ::Subchapter:: *)
+(*Export Model*)
+
+
+Export["SRGAN4x trained on VOC.WXF", mainNet]
