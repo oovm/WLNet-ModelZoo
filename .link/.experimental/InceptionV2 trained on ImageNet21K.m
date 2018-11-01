8b6cf9a9a498958cb372e1e450480c5e4124b037
GalAster <galaster@foxmail.com>
2018-11-01 11:31:58 +0800

    模型::InceptionV2 trained on ImageNet21K[Part2]

--- .link/.experimental/InceptionV2 trained on ImageNet21K.m
+++ .link/.experimental/InceptionV2 trained on ImageNet21K.m
@@ -1,13 +1,14 @@
 (* ::Package:: *)
 
 SetDirectory@NotebookDirectory[];
-Needs["MXNetLink`"]
-Needs["NeuralNetworks`"]
+<< NeuralNetworks`
+<< MXNetLink`
+<< DeepMath`
 DateString[]
 
 
 (* ::Subitem:: *)
-(*Thu 1 Nov 2018 00:11:42*)
+(*Thu 1 Nov 2018 11:25:25*)
 
 
 (* ::Subchapter:: *)
@@ -40,32 +41,14 @@ getCN[name_String, p_Integer, s_Integer] := ConvolutionLayer[
 	"Weights" -> params["arg:conv_" <> name <> "_weight"],
 	"Biases" -> params["arg:conv_" <> name <> "_bias"],
 	"PaddingSize" -> p, "Stride" -> s
-]
+];
 getBN[name_String] := BatchNormalizationLayer[
 	"Epsilon" -> 1*^-5,
 	"Beta" -> params["arg:bn_" <> name <> "_beta"],
 	"Gamma" -> params["arg:bn_" <> name <> "_gamma"],
 	"MovingMean" -> params["aux:bn_" <> name <> "_moving_mean"],
 	"MovingVariance" -> params["aux:bn_" <> name <> "_moving_var"]
-]
-
-
-$head = NetChain@{
-	getCN["conv1", 3, 2],
-	getBN["conv1"] ,
-	ElementwiseLayer["ReLU"],
-	PoolingLayer[{3, 3}, "Stride" -> 2],
-	getCN["conv2red", 3, 2],
-	getBN["conv2red"] ,
-	ElementwiseLayer["ReLU"],
-	getCN["conv2", 3, 2],
-	getBN["conv2"] ,
-	ElementwiseLayer["ReLU"],
-	PoolingLayer[{3, 3}, "Stride" -> 2]
-}
-
-
-
+];
 
 
 getBlock[name_, p_, s_] := NetChain@{
@@ -73,27 +56,67 @@ getBlock[name_, p_, s_] := NetChain@{
 	getBN[name] ,
 	ElementwiseLayer["ReLU"]
 };
-path1 = getBlock["3a_1x1", 0, 1]
-path2 = NetChain@{
-	getBlock["3a_3x3_reduce", 0, 1],
-	getBlock["3a_3x3", 1, 1]
-} // NetFlatten
-path3 = NetChain@{
-	getBlock["3a_double_3x3_reduce", 0, 1],
-	getBlock["3a_double_3x3_0", 1, 1],
-	getBlock["3a_double_3x3_1", 1, 1]
-} // NetFlatten
-path4 = NetChain@{
-	PoolingLayer[{3, 3}, "PaddingSize" -> 1, "Function" -> Mean],
-	getBlock["3a_proj", 0, 1]
-} // NetFlatten
+getBlock2[name_String] := Block[
+	{path1, path2, path3, path4},
+	path1 = getBlock[name <> "_1x1", 0, 1];
+	path2 = NetChain@{
+		getBlock[name <> "_3x3_reduce", 0, 1],
+		getBlock[name <> "_3x3", 1, 1]
+	} // NetFlatten;
+	path3 = NetChain@{
+		getBlock[name <> "_double_3x3_reduce", 0, 1],
+		getBlock[name <> "_double_3x3_0", 1, 1],
+		getBlock[name <> "_double_3x3_1", 1, 1]
+	} // NetFlatten;
+	path4 = NetChain@{
+		PoolingLayer[{3, 3}, "PaddingSize" -> 1, "Function" -> Mean],
+		getBlock[name <> "_proj", 0, 1]
+	} // NetFlatten;
+	NetMerge[{path1, path2, path3, path4}, Join, Expand -> All]
+];
+getBlock3[name_String] := Block[
+	{path1, path2, path3},
+	path1 = NetChain@{
+		getBlock[name <> "_3x3_reduce", 0, 1],
+		getBlock[name <> "_3x3", 1, 2]
+	} // NetFlatten;
+	path2 = NetChain@{
+		getBlock[name <> "_double_3x3_reduce", 0, 1],
+		getBlock[name <> "_double_3x3_0", 1, 1],
+		getBlock[name <> "_double_3x3_1", 1, 2]
+	} // NetFlatten;
+	path3 = PoolingLayer[{3, 3}, "PaddingSize" -> 1, "Stride" -> 2];
+	NetMerge[{path1, path2, path3}, Join, Expand -> All]
+];
 
 
 (* ::Subchapter:: *)
 (*Main*)
 
 
-
+mainNet = NetChain[{
+	getBlock["conv1", 3, 2],
+	PoolingLayer[{3, 3}, "Stride" -> 2],
+	getBlock["conv2red", 0, 1],
+	getBlock["conv2", 1, 1],
+	PoolingLayer[{3, 3}, "Stride" -> 2],
+	NetChain@{getBlock2["3a"], getBlock2["3b"]},
+	getBlock3["3c"],
+	NetChain@{getBlock2["4a"], getBlock2["4b"], getBlock2["4c"], getBlock2["4d"]},
+	getBlock3["4e"],
+	NetChain@{getBlock2["5a"], getBlock2["5b"]},
+	AggregationLayer[Mean],
+	{
+		LinearLayer[
+			"Weights" -> params["arg:fc1_weight"],
+			"Biases" -> params["arg:fc1_bias"]
+		],
+		SoftmaxLayer[]
+	}
+},
+	"Input" -> encoder,
+	"Output" -> decoder
+]
 
 
 (* ::Subchapter:: *)
