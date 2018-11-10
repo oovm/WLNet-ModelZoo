6f062f8ac760621fb2ab216b9ccc940d2f1f80fc
GalAster <galaster@foxmail.com>
2018-11-10 13:53:38 +0800

    模型::PGGAN trained on CelebA[Part2]

--- .link/PGGAN on CelebA/PGGAN trained on CelebA.m
+++ .link/PGGAN on CelebA/PGGAN trained on CelebA.m
@@ -23,14 +23,7 @@ params = Import@"karras2018iclr-celebahq-1024x1024.wxf";
 
 
 encoder = ""
-decoder = "Image"
-
-"Use this encoder if not use shift convolution";
-NetEncoder[{
-	"Image", {640, 360},
-	"MeanImage" -> {0.4488, 0.4371, 0.4040},
-	"VarianceImage" -> 1 / 255^2
-}];
+decoder = NetDecoder["Image"]
 
 
 (* ::Subchapter:: *)
@@ -42,20 +35,22 @@ $NCHW = TransposeLayer[{1<->4, 2<->3, 3<->4}];
 getWeight[name_String] := Block[
 	{raw = Normal@params[name]},
 	$NCHW[raw] / Dimensions[raw][[3]]
-]
+];
 getCN[name_, p_, s_] := ConvolutionLayer[
 	"Weights" -> $NCHW[Normal@params[name <> "/weight"]] / Sqrt@s,
 	"Biases" -> params[name <> "/bias"],
 	"PaddingSize" -> p, "Stride" -> 1
-]
-getOut[res_] := Block[
-	{i, weight},
+];
+getOut[res_, wScale_] := Block[
+	{i, weight, bias},
 	i = ToString[10 - Log2[res]];
-	weight = Normal[params["G_paper_1/ToRGB_lod" <> i <> "/weight"]];
+	weight = params["G_paper_1/ToRGB_lod" <> i <> "/weight"];
+	bias = params["G_paper_1/ToRGB_lod" <> i <> "/bias"];
 	ConvolutionLayer[
-		"Weights" -> $NCHW[weight] / Dimensions[weight][[3]],
-		"Biases" -> params["G_paper_1/ToRGB_lod" <> i <> "/bias"]]
-]
+		"Weights" -> $NCHW[Normal@weight] / Sqrt[4wScale],
+		"Biases" -> (Normal@bias + 1) / 2
+	]
+];
 getBlock[i_, s1_, s2_] := NetChain[{
 	ResizeLayer[Scaled /@ {2, 2}, "Resampling" -> "Nearest"],
 	getCN[StringRiffle[{"G_paper_1/", i, "x", i, "/Conv0"}, ""], 1, s1],
@@ -64,23 +59,17 @@ getBlock[i_, s1_, s2_] := NetChain[{
 	getCN[StringRiffle[{"G_paper_1/", i, "x", i, "/Conv1"}, ""], 1, s2],
 	leakyReLU[0.2],
 	PixelNormalizationLayer[]
-}]
+}];
 
 
-$head = NetChain@{
-(*ConvolutionLayer[
-"Weights"\[Rule]ReshapeLayer[{512,512,4,4}][
-Normal@params["G_paper_1/4x4/Dense/weight"]/64
-],
-"Biases"\[Rule]params["G_paper_1/4x4/Conv/bias"],
-"PaddingSize"\[Rule]3,"Stride"\[Rule]1
-],*)
+$part1 = NetChain@{
+	ReshapeLayer[{512, 4, 4}],
 	leakyReLU[0.2],
 	PixelNormalizationLayer[],
 	getCN["G_paper_1/4x4/Conv", 1, 1],
 	leakyReLU[0.2],
 	PixelNormalizationLayer[]
-}
+};
 
 
 (* ::Subchapter:: *)
@@ -88,7 +77,14 @@ Normal@params["G_paper_1/4x4/Dense/weight"]/64
 
 
 mainNet = NetChain[{
-	$head, (*I can't understand how to calc this*)
+(*ReshapeLayer[{512,1,1}],*)
+	LinearLayer[8192,
+		"Weights" -> Transpose[weight / 64],
+	(*there's no broadcast in Mathematica*)
+		"Biases" -> Flatten@TransposeLayer[{1<->2}][ConstantArray[bias, 16]]
+	],
+	$part1,
+(*I can't understand how to calc wScale*)
 	getBlock[8, 2304, 2304],
 	getBlock[16, 2304, 2304],
 	getBlock[32, 2304, 2304],
@@ -97,9 +93,10 @@ mainNet = NetChain[{
 	getBlock[256, 576, 288],
 	getBlock[512, 288, 144],
 	getBlock[1024, 144, 72],
-	getOut[1024]
+	getOut[1024, 16]
+(*ResizeLayer[Scaled/@{1/3,1/3}]*)
 },
-	"Input" -> {512, 4, 4},
+	"Input" -> 512,
 	"Output" -> "Image"
 ]
 
