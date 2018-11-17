af3933968ca5bfc084b64f87076d72f6749c6938
GalAster <galaster@foxmail.com>
2018-11-17 23:41:39 +0800

    测试::Test for WRN16-10 Trained on CIFAR10

--- ImageRecognition/ImageClassifation/Wide-ResNet on CIFAR10/WRN28-10 trained on CIFAR10.m
+++ ImageRecognition/ImageClassifation/Wide-ResNet on CIFAR10/WRN28-10 trained on CIFAR10.m
@@ -28,7 +28,7 @@ tags = {"airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse",
 decoder = NetDecoder[{"Class", tags}]
 
 
-(* ::Subchapter:: *)
+(* ::Subchapter::Closed:: *)
 (*Pre-defined Structure*)
 
 
@@ -38,22 +38,22 @@ getBN[i_, j_] := BatchNormalizationLayer[
 	"Gamma" -> params["arg:cifarwideresnet1_stage" <> i <> "_batchnorm" <> j <> "_gamma"],
 	"MovingMean" -> params["aux:cifarwideresnet1_stage" <> i <> "_batchnorm" <> j <> "_running_mean"],
 	"MovingVariance" -> params["aux:cifarwideresnet1_stage" <> i <> "_batchnorm" <> j <> "_running_var"]
-]
+];
 getBN2[j_] := BatchNormalizationLayer[
 	"Epsilon" -> 1*^-5,
 	"Beta" -> params["arg:cifarwideresnet1_batchnorm" <> j <> "_beta"],
 	"Gamma" -> params["arg:cifarwideresnet1_batchnorm" <> j <> "_gamma"],
 	"MovingMean" -> params["aux:cifarwideresnet1_batchnorm" <> j <> "_running_mean"],
 	"MovingVariance" -> params["aux:cifarwideresnet1_batchnorm" <> j <> "_running_var"]
-]
+];
 getCN[i_, j_, p_ : 1, s_ : 1] := ConvolutionLayer[
 	"Weights" -> params["arg:cifarwideresnet1_stage" <> i <> "_conv" <> j <> "_weight"],
 	"Biases" -> None, "PaddingSize" -> p, "Stride" -> s
-]
+];
 getCN2[j_] := ConvolutionLayer[
 	"Weights" -> params["arg:cifarwideresnet1_conv" <> j <> "_weight"],
 	"Biases" -> None, "PaddingSize" -> 1
-]
+];
 getBlock[i_, j_, k_ : 0] := NetGraph[{
 	getBN[ToString[i], ToString[j]], Ramp, getCN[ToString[i], ToString[j + k]],
 	getBN[ToString[i], ToString[j + 1]], Ramp, getCN[ToString[i], ToString[j + 1 + k]],
@@ -61,32 +61,28 @@ getBlock[i_, j_, k_ : 0] := NetGraph[{
 }, {
 	NetPort["Input"] -> 7,
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
-}]
+}];
 getBlock2[i_] := NetGraph[{
 	getBN[ToString[i], "0"], Ramp, getCN[ToString[i], "2", 0, 1], ThreadingLayer[Plus],
 	getCN[ToString[i], "0"], getBN[ToString[i], "1"], Ramp, getCN[ToString[i], "1"]
 }, {
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4,
 	2 -> 5 -> 6 -> 7 -> 8 -> 4
-}]
+}];
 getBlock3[i_] := NetGraph[{
 	getBN[ToString[i], "0"], Ramp, getCN[ToString[i], "2", 0, 2], ThreadingLayer[Plus],
 	getCN[ToString[i], "0", 1, 2], getBN[ToString[i], "1"], Ramp, getCN[ToString[i], "1"]
 }, {
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4,
 	2 -> 5 -> 6 -> 7 -> 8 -> 4
-}]
-getBlock4[] := NetChain[{
-	getBN2["2"], Ramp,
-	AggregationLayer[Mean]
-}]
+}];
 
 
 (* ::Subchapter:: *)
 (*Main*)
 
 
-mainNet = NetChain[{
+extractor = NetChain[{
 	NetChain[{getBN2["0"] , getCN2["0"], getBN2["1"]}],
 	getBlock2[1],
 	NetChain@Table[getBlock[1, i, 1], {i, 2, 6, 2}],
@@ -94,20 +90,39 @@ mainNet = NetChain[{
 	NetChain@Table[getBlock[2, i, 1], {i, 2, 6, 2}],
 	getBlock3[3],
 	NetChain@Table[getBlock[3, i, 1], {i, 2, 6, 2}],
-	getBlock4[],
-	LinearLayer[10,
-		"Weights" -> params["arg:cifarwideresnet1_dense0_weight"],
-		"Biases" -> params["arg:cifarwideresnet1_dense0_bias"]
-	],
-	SoftmaxLayer[]
+	getBN2["2"],
+	ElementwiseLayer["ReLU"],
+	AggregationLayer[Mean]
+}];
+classifier = LinearLayer[10,
+	"Weights" -> params["arg:cifarwideresnet1_dense0_weight"],
+	"Biases" -> params["arg:cifarwideresnet1_dense0_bias"]
+];
+mainNet = NetChain[{
+	"Extractor" -> extractor,
+	"Classifier" -> classifier,
+	"Predictor" -> SoftmaxLayer[]
 },
 	"Input" -> encoder,
 	"Output" -> decoder
 ]
 
-
 (* ::Subchapter:: *)
 (*Export Model*)
 
 
-Export["cifar_wideresnet28_10.WXF", mainNet]
+
+
+
+Export["WRN28-10 trained on CIFAR10.WXF", mainNet]
+
+
+(* ::Subchapter:: *)
+(*Test*)
+
+
+test = TestReport["WRN28-10 trained on CIFAR10.mt"]
+ClassifyAnalyzeExport[analyze, test]
+
+
+
--- ImageRecognition/ImageClassifation/Wide-ResNet on CIFAR10/WRN40-8 trained on CIFAR10.m
+++ ImageRecognition/ImageClassifation/Wide-ResNet on CIFAR10/WRN40-8 trained on CIFAR10.m
@@ -28,7 +28,7 @@ tags = {"airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse",
 decoder = NetDecoder[{"Class", tags}]
 
 
-(* ::Subchapter:: *)
+(* ::Subchapter::Closed:: *)
 (*Pre-defined Structure*)
 
 
@@ -38,14 +38,14 @@ getBN[i_, j_] := BatchNormalizationLayer[
 	"Gamma" -> params["arg:cifarwideresnet2_stage" <> i <> "_batchnorm" <> j <> "_gamma"],
 	"MovingMean" -> params["aux:cifarwideresnet2_stage" <> i <> "_batchnorm" <> j <> "_running_mean"],
 	"MovingVariance" -> params["aux:cifarwideresnet2_stage" <> i <> "_batchnorm" <> j <> "_running_var"]
-]
+];
 getBN2[j_] := BatchNormalizationLayer[
 	"Epsilon" -> 1*^-5,
 	"Beta" -> params["arg:cifarwideresnet2_batchnorm" <> j <> "_beta"],
 	"Gamma" -> params["arg:cifarwideresnet2_batchnorm" <> j <> "_gamma"],
 	"MovingMean" -> params["aux:cifarwideresnet2_batchnorm" <> j <> "_running_mean"],
 	"MovingVariance" -> params["aux:cifarwideresnet2_batchnorm" <> j <> "_running_var"]
-]
+];
 getCN[i_, j_, p_ : 1, s_ : 1] := ConvolutionLayer[
 	"Weights" -> params["arg:cifarwideresnet2_stage" <> i <> "_conv" <> j <> "_weight"],
 	"Biases" -> None, "PaddingSize" -> p, "Stride" -> s
@@ -53,7 +53,7 @@ getCN[i_, j_, p_ : 1, s_ : 1] := ConvolutionLayer[
 getCN2[j_] := ConvolutionLayer[
 	"Weights" -> params["arg:cifarwideresnet2_conv" <> j <> "_weight"],
 	"Biases" -> None, "PaddingSize" -> 1
-]
+];
 getBlock[i_, j_, k_ : 0] := NetGraph[{
 	getBN[ToString[i], ToString[j]], Ramp, getCN[ToString[i], ToString[j + k]],
 	getBN[ToString[i], ToString[j + 1]], Ramp, getCN[ToString[i], ToString[j + 1 + k]],
@@ -61,32 +61,28 @@ getBlock[i_, j_, k_ : 0] := NetGraph[{
 }, {
 	NetPort["Input"] -> 7,
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
-}]
+}];
 getBlock2[i_] := NetGraph[{
 	getBN[ToString[i], "0"], Ramp, getCN[ToString[i], "2", 0, 1], ThreadingLayer[Plus],
 	getCN[ToString[i], "0"], getBN[ToString[i], "1"], Ramp, getCN[ToString[i], "1"]
 }, {
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4,
 	2 -> 5 -> 6 -> 7 -> 8 -> 4
-}]
+}];
 getBlock3[i_] := NetGraph[{
 	getBN[ToString[i], "0"], Ramp, getCN[ToString[i], "2", 0, 2], ThreadingLayer[Plus],
 	getCN[ToString[i], "0", 1, 2], getBN[ToString[i], "1"], Ramp, getCN[ToString[i], "1"]
 }, {
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4,
 	2 -> 5 -> 6 -> 7 -> 8 -> 4
-}]
-getBlock4[] := NetChain[{
-	getBN2["2"], Ramp,
-	AggregationLayer[Mean]
-}]
+}];
 
 
 (* ::Subchapter:: *)
 (*Main*)
 
 
-mainNet = NetChain[{
+extractor = NetChain[{
 	NetChain[{getBN2["0"] , getCN2["0"], getBN2["1"]}],
 	getBlock2[1],
 	NetChain@Table[getBlock[1, i, 1], {i, 2, 10, 2}],
@@ -94,12 +90,18 @@ mainNet = NetChain[{
 	NetChain@Table[getBlock[2, i, 1], {i, 2, 10, 2}],
 	getBlock3[3],
 	NetChain@Table[getBlock[3, i, 1], {i, 2, 10, 2}],
-	getBlock4[],
-	LinearLayer[10,
-		"Weights" -> params["arg:cifarwideresnet2_dense0_weight"],
-		"Biases" -> params["arg:cifarwideresnet2_dense0_bias"]
-	],
-	SoftmaxLayer[]
+	getBN2["2"],
+	ElementwiseLayer["ReLU"],
+	AggregationLayer[Mean]
+}];
+classifier = LinearLayer[10,
+	"Weights" -> params["arg:cifarwideresnet2_dense0_weight"],
+	"Biases" -> params["arg:cifarwideresnet2_dense0_bias"]
+];
+mainNet = NetChain[{
+	"Extractor" -> extractor,
+	"Classifier" -> classifier,
+	"Predictor" -> SoftmaxLayer[]
 },
 	"Input" -> encoder,
 	"Output" -> decoder
@@ -110,4 +112,13 @@ mainNet = NetChain[{
 (*Export Model*)
 
 
-Export["cifar_wideresnet40_8.WXF", mainNet]
+Export["WRN40-8 trained on CIFAR10.WXF", mainNet]
+
+
+(* ::Subchapter:: *)
+(*Test*)
+
+
+test = TestReport["WRN40-8 trained on CIFAR10.mt"]
+ClassifyAnalyzeExport[analyze, test]
+
