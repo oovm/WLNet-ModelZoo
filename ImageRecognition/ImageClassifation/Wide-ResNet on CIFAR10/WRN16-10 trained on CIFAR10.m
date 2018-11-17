64eda979c3c532aad5b1ed61a50386c77b7d56ed
GalAster <galaster@foxmail.com>
2018-11-17 23:36:10 +0800

    测试::Prepare test for WRN16-10 Trained on CIFAR10

--- ImageRecognition/ImageClassifation/Wide-ResNet on CIFAR10/WRN16-10 trained on CIFAR10.m
+++ ImageRecognition/ImageClassifation/Wide-ResNet on CIFAR10/WRN16-10 trained on CIFAR10.m
@@ -1,13 +1,14 @@
 (* ::Package:: *)
 
 SetDirectory@NotebookDirectory[];
-Needs["MXNetLink`"]
-Needs["NeuralNetworks`"]
+<< NeuralNetworks`
+<< MXNetLink`
+<< DeepMath`
 DateString[]
 
 
 (* ::Subitem:: *)
-(*Tue 16 Oct 2018 20:31:45*)
+(*Sat 17 Nov 2018 23:11:06*)
 
 
 (* ::Subchapter:: *)
@@ -28,7 +29,7 @@ tags = {"airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse",
 decoder = NetDecoder[{"Class", tags}]
 
 
-(* ::Subchapter:: *)
+(* ::Subchapter::Closed:: *)
 (*Pre-defined Structure*)
 
 
@@ -38,22 +39,22 @@ getBN[i_, j_] := BatchNormalizationLayer[
 	"Gamma" -> params["arg:cifarwideresnet0_stage" <> i <> "_batchnorm" <> j <> "_gamma"],
 	"MovingMean" -> params["aux:cifarwideresnet0_stage" <> i <> "_batchnorm" <> j <> "_running_mean"],
 	"MovingVariance" -> params["aux:cifarwideresnet0_stage" <> i <> "_batchnorm" <> j <> "_running_var"]
-]
+];
 getBN2[j_] := BatchNormalizationLayer[
 	"Epsilon" -> 1*^-5,
 	"Beta" -> params["arg:cifarwideresnet0_batchnorm" <> j <> "_beta"],
 	"Gamma" -> params["arg:cifarwideresnet0_batchnorm" <> j <> "_gamma"],
 	"MovingMean" -> params["aux:cifarwideresnet0_batchnorm" <> j <> "_running_mean"],
 	"MovingVariance" -> params["aux:cifarwideresnet0_batchnorm" <> j <> "_running_var"]
-]
+];
 getCN[i_, j_, p_ : 1, s_ : 1] := ConvolutionLayer[
 	"Weights" -> params["arg:cifarwideresnet0_stage" <> i <> "_conv" <> j <> "_weight"],
 	"Biases" -> None, "PaddingSize" -> p, "Stride" -> s
-]
+];
 getCN2[j_] := ConvolutionLayer[
 	"Weights" -> params["arg:cifarwideresnet0_conv" <> j <> "_weight"],
 	"Biases" -> None, "PaddingSize" -> 1
-]
+];
 getBlock[i_, j_, k_ : 0] := NetGraph[{
 	getBN[ToString[i], ToString[j]], Ramp, getCN[ToString[i], ToString[j + k]],
 	getBN[ToString[i], ToString[j + 1]], Ramp, getCN[ToString[i], ToString[j + 1 + k]],
@@ -61,46 +62,50 @@ getBlock[i_, j_, k_ : 0] := NetGraph[{
 }, {
 	NetPort["Input"] -> 7,
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
-}]
+}];
 getBlock2[i_] := NetGraph[{
 	getBN[ToString[i], "0"], Ramp, getCN[ToString[i], "2", 0, 1], ThreadingLayer[Plus],
 	getCN[ToString[i], "0"], getBN[ToString[i], "1"], Ramp, getCN[ToString[i], "1"]
 }, {
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4,
 	2 -> 5 -> 6 -> 7 -> 8 -> 4
-}]
+}];
 getBlock3[i_] := NetGraph[{
 	getBN[ToString[i], "0"], Ramp, getCN[ToString[i], "2", 0, 2], ThreadingLayer[Plus],
 	getCN[ToString[i], "0", 1, 2], getBN[ToString[i], "1"], Ramp, getCN[ToString[i], "1"]
 }, {
 	NetPort["Input"] -> 1 -> 2 -> 3 -> 4,
 	2 -> 5 -> 6 -> 7 -> 8 -> 4
-}]
-getBlock4[] := NetChain[{
-	getBN2["2"], Ramp,
-	AggregationLayer[Mean]
-}]
+}];
 
 
 (* ::Subchapter:: *)
 (*Main*)
 
 
-mainNet = NetChain[{
-	NetChain[{getBN2["0"] , getCN2["0"], getBN2["1"]}],
+extractor = NetChain[{
+	getBN2["0"] ,
+	getCN2["0"],
+	getBN2["1"],
 	getBlock2[1],
 	Table[getBlock[1, 2, 1], {i, 2, 2, 2}],
 	getBlock3[2],
 	Table[getBlock[2, i, 1], {i, 2, 2, 2}],
 	getBlock3[3],
 	Table[getBlock[3, i, 1], {i, 2, 2, 2}],
-	getBlock4[],
-	LinearLayer[10,
-		"Weights" -> params["arg:cifarwideresnet0_dense0_weight"],
-		"Biases" -> params["arg:cifarwideresnet0_dense0_bias"]
-	],
-	SoftmaxLayer[]
-} // Flatten,
+	getBN2["2"],
+	ElementwiseLayer["ReLU"],
+	AggregationLayer[Mean]
+}];
+classifier = LinearLayer[10,
+	"Weights" -> params["arg:cifarwideresnet0_dense0_weight"],
+	"Biases" -> params["arg:cifarwideresnet0_dense0_bias"]
+];
+mainNet = NetChain[{
+	"Extractor" -> extractor,
+	"Classifier" -> classifier,
+	"Predictor" -> SoftmaxLayer[]
+},
 	"Input" -> encoder,
 	"Output" -> decoder
 ]
@@ -110,4 +115,15 @@ mainNet = NetChain[{
 (*Export Model*)
 
 
-Export["cifar_wideresnet16_10.WXF", mainNet]
+Export["WRN16-10 trained on CIFAR10.WXF", mainNet]
+
+
+(* ::Subchapter:: *)
+(*Test*)
+
+
+test = TestReport["WRN16-10 trained on CIFAR10.mt"]
+ClassifyAnalyzeExport[analyze, test]
+
+
+
