289680982ae72eb954147dc2012323eb39984fb1
GalAster <galaster@foxmail.com>
2018-11-24 00:06:43 +0800

    目录::Adjust directory structure

---
+++ ImageRecognition/Classifation/RAN on CIFAR10/exporter.py
@@ -0,0 +1,13 @@
+import gluoncv.model_zoo as gz
+from gluoncv.utils import export_block
+
+
+def zoo_import(name, head=''):
+    """Download from Gluoncv Zoo"""
+    net = gz.get_model(name, pretrained=True)
+    export_block(head + name, net, preprocess=True)
+
+
+zoo_import('cifar_residualattentionnet56')
+zoo_import('cifar_residualattentionnet92')
+zoo_import('cifar_residualattentionnet452')
